14a937ebc61a030d12a24449d5ead973acd34156
Andriy Pustovit <andriy.pustovit@mail.ru>
2022-06-03 15:59:33 +0300

    ddd

---
+++ dfdf.js
@@ -0,0 +1,26 @@
+// const option = 3;
+// let message = "";
+
+// switch (option) {
+//     case 1:
+//         message = "Call back";
+//         break;
+    
+//     case 2:
+//         message = "call again";
+//         break;
+       
+//     case 3:
+//         message = "call no";
+//         break;
+
+//     default:
+//         message = "we will back";
+// }
+
+// console.log(message);
+
+for (let i = 1; i < 10; i += 1) {
+    console.log(i);
+}
+console.log("qeqweqeq");
