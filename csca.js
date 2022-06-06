3f2ee8cb581ef23cdc194502543afbfd4ed46104
Andriy Pustovit <andriy.pustovit@mail.ru>
2022-06-06 21:43:39 +0300

    іі

---
+++ csca.js
@@ -0,0 +1,10 @@
+// let quaon = prompt(`вывывы`);
+// quaon = Number(quaon);
+// console.log(quaon);
+// console.log(typeof quaon);
+
+// let elem
+
+const balance = 10;
+const messege = balance >= 100 ? 'позитивний баланс' : 'негативний баланс';   
+console.log(messege);
--- dfdf.js
+++ dfdf.js
@@ -1,26 +1,59 @@
-// const option = 3;
-// let message = "";
+// // const option = 3;
+// // let message = "";
 
-// switch (option) {
-//     case 1:
-//         message = "Call back";
-//         break;
+// // switch (option) {
+// //     case 1:
+// //         message = "Call back";
+// //         break;
     
-//     case 2:
-//         message = "call again";
-//         break;
+// //     case 2:
+// //         message = "call again";
+// //         break;
        
-//     case 3:
-//         message = "call no";
-//         break;
+// //     case 3:
+// //         message = "call no";
+// //         break;
 
-//     default:
-//         message = "we will back";
+// //     default:
+// //         message = "we will back";
+// // }
+
+// // console.log(message);
+
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i);
 // }
+// console.log("qeqweqeq");
 
+// const isSidebarOpen = 44;
+// console.log("sss",isSidebarOpen);
+
+// console.log (Math.max(20, 10, 50, 40)); // 10
+
+// const message = "Mango " + "is" + " happy";
 // console.log(message);
 
-for (let i = 1; i < 10; i += 1) {
-    console.log(i);
-}
-console.log("qeqweqeq");
+// const guestName = "Манго";
+// const roomNumber = 207;
+// const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+// console.log(greeting);
+
+// const cssFileNames = "styles.css, about.css, portfolio.css";
+// const minifiedCssFileNames = cssFileNames.replaceAll(".css", ".min.css");
+// console.log(minifiedCssFileNames);
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+
+// const message = ` welcome ${hh}` ;
+// console.log(message);
+
+
+
+
+// const guestName = "Манго";
+// const roomNumber = 207;
+
+// const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+// console.log(greeting);
